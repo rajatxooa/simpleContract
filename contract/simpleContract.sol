92ebd100fcb5785aff7793e0be11a822aa592e2a
Rajat Shamra <rajat.sharma@xooa.com>
2018-10-25 12:53:20 +0530

    get set

--- contract/simpleContract.sol
+++ contract/simpleContract.sol
@@ -1,7 +1,19 @@
+pragma solidity ^0.4.0;
+
 contract SimpleContract{
 
     string h = "hello";
-    function g() constant returns(string) {
+    mapping (string => string) arguments;
+
+    function get(string[] args) constant returns(string) {
+        return arguments[args[0]];
+    }
+
+    function set(string[] args) public {
+        arguments[args[0]] = args[1];
+    }
+
+    function getHelloText() constant returns(string) {
         return h;
-    } 
+    }
 }
